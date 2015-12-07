86115e87962446c38a29c06ef7fc7071e59b4ff0
Ben Tranter <bwtrante@lakeheadu.ca>
2015-12-06 23:00:45 -0500

    Add test for  method

--- password_test.go
+++ password_test.go
@@ -1,22 +1,25 @@
 package password
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"golang.org/x/net/context"
 )
 
 var (
 	storedPassword = ""
-	key            = []byte("The quick brown fox jumps over the lazy dog")
+	key            = []byte("secret")
 	tokJSON        = ""
 )
 
 type tokStruct struct {
-	token string
+	Token string `json:"token"`
 }
 
 type mock struct {
@@ -86,7 +89,7 @@ func TestAuthenticate(t *testing.T) {
 	body, err := ioutil.ReadAll(w.Body)
 
 	if err != nil {
-		t.Errorf("Couldn't read recorded response body")
+		t.Errorf("Couldn't read recorded response body: %s\n", err)
 	}
 	if len(body) == 0 {
 		t.Errorf("Response body is an empty string")
@@ -96,5 +99,32 @@ func TestAuthenticate(t *testing.T) {
 }
 
 func TestProtected(t *testing.T) {
+	ts := httptest.NewServer(Protected(authReq))
+	defer ts.Close()
 
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", ts.URL, nil)
+	if err != nil {
+		t.Errorf("Couldn't create new test request: %s\n", err)
+	}
+
+	var tok tokStruct
+	json.Unmarshal([]byte(tokJSON), &tok)
+	req.Header.Set("Authorization", "Bearer "+tok.Token)
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to complete mocked POST request: %s\n", err)
+	}
+	res.Body.Close()
+
+	status := res.StatusCode
+	if status != 200 {
+		t.Errorf("Expected response status to be 200, got %d\n", status)
+	}
+}
+
+func authReq(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	user := ctx.Value("id")
+	w.Write([]byte("User: " + user.(string)))
 }
