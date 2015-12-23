c73d940ad4711eda8dea1c8b7ee2f88c26d58098
Ben Tranter <bwtrante@lakeheadu.ca>
2015-12-22 22:52:24 -0500

    Remove unused methods from example

--- cmd/example.go
+++ cmd/example.go
@@ -1,63 +1,23 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/hex"
-	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
-	"sync"
 
 	"github.com/bentranter/password"
 	"golang.org/x/net/context"
 )
 
-var (
-	db = newInMemDB()
-)
-
-type inMemDB struct {
-	rwm *sync.RWMutex
-	m   map[string]string
-}
-
-func newInMemDB() *inMemDB {
-	return &inMemDB{
-		rwm: &sync.RWMutex{},
-		m:   make(map[string]string),
-	}
-}
-
-func (db *inMemDB) Store(id string, hashedPassword string) (string, error) {
-	db.rwm.Lock()
-	defer db.rwm.Unlock()
-	key := genKey()
-	db.m[key] = hashedPassword
-	return key, nil
-}
-
-func (db *inMemDB) Retrieve(id string) (string, error) {
-	db.rwm.RLock()
-	defer db.rwm.RUnlock()
-	return db.m[id], nil
-}
-
 type user struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string
+	Password string
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
-	var u user
-	json.NewDecoder(r.Body).Decode(&u)
-
-	id, err := password.New(u.Username, u.Password, db)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "{\n\tid: %s\n}\n", id)
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	r.ParseForm()
 }
 
 func comparePwd(w http.ResponseWriter, r *http.Request) {
@@ -79,13 +39,4 @@ func main() {
 	mux.Handle("/user", password.Protect(authReq))
 
 	http.ListenAndServe(":3000", mux)
-}
-
-func genKey() string {
-	b := make([]byte, 8)
-	_, err := rand.Read(b)
-	if err != nil {
-		panic(err)
-	}
-	return hex.EncodeToString(b)
 }
