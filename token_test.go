5ccdc8c302f07c4c24ec1d1008d9d47b5bd39155
Ben Tranter <bwtrante@lakeheadu.ca>
2016-01-27 22:20:29 -0500

    Add some more tests

--- token_test.go
+++ token_test.go
@@ -2,6 +2,13 @@ package password
 
 import "testing"
 
+func Test_GenToken(t *testing.T) {
+	_, err := GenToken("1")
+	if err != nil {
+		t.Errorf("Failed: Error whle genrating token. %s\n", err.Error())
+	}
+}
+
 func Test_genRandBytes(t *testing.T) {
 	l := len(genRandBytes())
 	if l != 32 {
