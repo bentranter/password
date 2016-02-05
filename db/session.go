1142603406ef316ac312d4a2921c111b8c6b0e10
Ben Tranter <bwtrante@lakeheadu.ca>
2016-02-04 23:34:54 -0500

    Trying new idea

---
+++ db/session.go
@@ -0,0 +1,9 @@
+package db
+
+// BoltSession is the struct that holds the data for our Bolt sessions
+type BoltSession struct {
+}
+
+// RedisSession is the struct that holds the data for our Redis sessions
+type RedisSession struct {
+}
