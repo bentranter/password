baa982f40cca990deb2821df5917196df7da4a5a
Ben Tranter <bwtrante@lakeheadu.ca>
2016-02-05 23:47:24 -0500

    Setup default BoltDB

--- user.go
+++ user.go
@@ -1,11 +1,13 @@
 package password
 
 import (
+	"time"
+
 	"github.com/boltdb/bolt"
 	"github.com/garyburd/redigo/redis"
 )
 
-var userStore = ""
+var userStore = NewBoltUserStore()
 
 // UserStore stores users in DBs
 type UserStore interface {
@@ -38,3 +40,26 @@ func (u *RedisUser) Create() {}
 func (u *RedisUser) Find()   {}
 func (u *RedisUser) Update() {}
 func (u *RedisUser) Delete() {}
+
+// NewBoltUserStore creates a new instance of BoltUser.
+func NewBoltUserStore() *BoltUser {
+	db, err := bolt.Open("boltuser.db", 0600, &bolt.Options{
+		Timeout: 1 * time.Second,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("boltuserbucket"))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return &BoltUser{
+		DB:         db,
+		BucketName: []byte("boltuserbucket"),
+	}
+}
